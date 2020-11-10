48adaab5494e511e76c0da9c3cdae2f80a673f67
Jim Wang <54686917+jiwang6@users.noreply.github.com>
2020-11-10 11:30:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,16 @@ var date = new Date();
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/ahaha$/;
+      botRegex0 = /^\/ahaha$/,
+	  botRegex1 = /^\/sendushome$/;
 
-  if (request.text && botRegex.test(request.text)) { 
+  if (request.text && botRegex0.test(request.text)) { 
     this.res.writeHead(200);
-    postMessage();
+    postMessage(0);
+    this.res.end();
+  } else if(request.text && botRegex1.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage1(1);
     this.res.end();
   } else {
     console.log("don't care");
@@ -21,10 +26,46 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postMessage(var choice) {
   var botResponse, options, body, botReq;
 
-  botResponse = newJoke();
+  if (choice == 0) {
+	  botResponse = newJoke();
+  } else if (choice == 1) {
+	  botResponse = 'no, stop asking or I\'ll open an honor case against you.';
+  }
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+  var botResponse, options, body, botReq;
+
+  botResponse = 'stop asking';
 
   options = {
     hostname: 'api.groupme.com',
